3be66aa96de1f77ebd0398c00f4644559a3280c9
Splinntz <antonin.chigot@gmail.com>
2019-02-11 11:26:40 +0100

    add routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,8 +8,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class HomeController
 {
-
+    /**
+     * @Route("/");
+     */
+    public function homepage() {
+        return new Response ('First page');
+    }
 }
