f0eb5ca90d9eef5630ccbb152a42943051f48ff9
Splinntz <antonin.chigot@gmail.com>
2019-02-11 12:05:00 +0100

    list article

--- src/Controller/Article.php
+++ src/Controller/Article.php
@@ -8,8 +8,17 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class Article
 {
-
+    /**
+     * @Route("/article/{article}");
+     * @param $article
+     * @return Response
+     */
+    public function list($article) {
+        return new Response ('First article : '.$article.'');
+    }
 }
